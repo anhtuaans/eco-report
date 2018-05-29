e571d8e74a416480b25f370296f3d8da346688c4
Anh Tuan <anhtuanbk90@gmail.com>
2018-05-29 21:32:47 +0900

    no message

--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 // List users
-Route::get('users', 'UserControler@index');
+Route::get('users', 'UserController@index');
