79d25ee8dce6abc6616b992e368440d99f3d200a
Anh Tuan <anhtuanbk90@gmail.com>
2018-05-29 22:08:15 +0900

    no message

--- app/Http/Resources/User.php
+++ app/Http/Resources/User.php
@@ -14,6 +14,11 @@ class User extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        // return parent::toArray($request);
+        return [
+            'id'    => $this->idusers,
+            'email' => $this->email,
+            'name'  => $this->name
+        ];
     }
 }
