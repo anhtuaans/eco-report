11f6f4512a7844b5f29962441ace12abaa1c6d34
Anh Tuan <anhtuanbk90@gmail.com>
2018-05-30 00:51:45 +0900

    no message

--- routes/api.php
+++ routes/api.php
@@ -17,5 +17,5 @@ Route::post('login', 'API\UserController@login');
 Route::post('register', 'API\UserController@register');
 
 Route::group(['middleware' => 'auth:api'], function() {
-    Route::post('detail', 'API\UserController@detail');
+    Route::post('details', 'API\UserController@details');
 });
