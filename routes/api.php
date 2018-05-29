81ecb109089ece5313076987d3559f1e04befc23
Anh Tuan <anhtuanbk90@gmail.com>
2018-05-30 00:27:35 +0900

    no message

--- routes/api.php
+++ routes/api.php
@@ -13,9 +13,9 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::post('login', 'API\UserController@login');
+Route::post('register', 'API\UserController@register');
 
-// List users
-Route::get('users', 'UserController@index');
+Route::group(['middleware' => 'auth:api'], function() {
+    Route::post('detail', 'API\UserController@detail');
+});
