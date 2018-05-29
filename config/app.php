1f2a522c69557e9369ccc73e26e3b606496f8d8e
Anh Tuan <anhtuanbk90@gmail.com>
2018-05-29 23:38:36 +0900

    no message

--- config/app.php
+++ config/app.php
@@ -208,7 +208,9 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-
+        'providers' =>[
+            Laravel\Passport\PassportServiceProvider::class,
+            ],
     ],
 
 ];
